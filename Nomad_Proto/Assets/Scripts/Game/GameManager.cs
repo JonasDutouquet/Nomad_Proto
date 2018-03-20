cc5e8a9e4b6c554196b8c3db2a5aa19038468748
FORMATION\Jonas.dutouquet <jonas.dutouquet@gmail.com>
2018-03-20 14:26:07 +0100

    Added windows load save support

--- Nomad_Proto/Assets/Scripts/Game/GameManager.cs
+++ Nomad_Proto/Assets/Scripts/Game/GameManager.cs
@@ -14,9 +14,15 @@ public class GameManager : MonoBehaviour
 
 	void Awake()
 	{
-		if(_playing) {
-			_loader.Load ("/Users/jonas/Library/Application Support/DefaultCompany/Nomad_Proto/"+ _saveToLoad +".map");
-			_hexUI.SetEditMode (false);
+		if(_playing)
+        {
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+            _loader.Load ("/Users/jonas/Library/Application Support/DefaultCompany/Nomad_Proto/"+ _saveToLoad +".map");
+#endif
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            _loader.Load("C:\\Users\\jonas.dutouquet\\AppData\\LocalLow\\DefaultCompany\\Nomad_Proto\\" +_saveToLoad + ".map");
+#endif
+            _hexUI.SetEditMode (false);
 			_mapEditor.SetEditMode (false);
 			_editModeUI.SetActive (false);
 			//_turn = GetComponent<TurnManager> ();
