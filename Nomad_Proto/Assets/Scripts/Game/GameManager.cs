4a63072e9ed6660b0f4fa6ee313382467187c24e
FORMATION\Jonas.dutouquet <jonas.dutouquet@gmail.com>
2018-03-21 16:47:08 +0100

    f

--- Nomad_Proto/Assets/Scripts/Game/GameManager.cs
+++ Nomad_Proto/Assets/Scripts/Game/GameManager.cs
@@ -17,11 +17,9 @@ public class GameManager : MonoBehaviour
 		if(_playing) {
 #if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
             _loader.Load ("/Users/jonas/Library/Application Support/DefaultCompany/Nomad_Proto/"+ _saveToLoad +".map");
-#endif
-#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
             _loader.Load("C:\\Users\\jonas.dutouquet\\AppData\\LocalLow\\DefaultCompany\\Nomad_Proto\\" +_saveToLoad + ".map");
 #endif
-			_loader.Load ("/Users/jonas/Library/Application Support/DefaultCompany/Nomad_Proto/"+ _saveToLoad +".map");
 			_hexUI.SetEditMode (false);
 			_mapEditor.SetEditMode (false);
 			_editModeUI.SetActive (false);
